bc4be13225be0328e637ffeb8bc647e4fd3e8272
Gregory <gamercoder215@teaminceptus.us>
2024-01-25 20:24:11 +0000

    Update Dependencies

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import * as fs from 'fs';
 
-try {} catch(error) {
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+import runtime from './assets/runtime';
+
+try {
+    const runtime = core.getInput('runtime', { required: true });
+    const path = core.getInput('path', { required: true });
+    
+    const dir = fs.mkdtempSync('server')
+    
+
+} catch(error) {
     core.setFailed(error.message);
 }
