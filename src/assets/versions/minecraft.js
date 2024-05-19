d3bf7914047f6cc79aa850e93c3ea5b4636036dc
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2024-05-19 16:30:48 -0500

    Add 1.20.6

--- src/assets/versions/minecraft.js
+++ src/assets/versions/minecraft.js
@@ -1,9 +1,10 @@
 import * as core from '@actions/core'
 
 export const versions = {
-    "latest": "1.20.5",
+    "latest": "1.20.6",
     "versions": [
         // Versions must be found from https://hub.spigotmc.org/versions/
+        "1.20.6",
         "1.20.5",
         "1.20.4",
         "1.20.3",
@@ -68,6 +69,7 @@ export const versions = {
         "1.8"
     ],
     "similar_versions": {
+        "1.20.6": ["1.20.5"],
         "1.20.4": ["1.20.3"],
         "1.20.1": ["1.20"],
 
