99aa9b9cb17d351cd1bbd831abb88ed5d32c4bfc
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2026-05-16 11:39:46 -0500

    chore: update versions

--- src/assets/versions/minecraft.js
+++ src/assets/versions/minecraft.js
@@ -1,142 +1,155 @@
-import * as core from '@actions/core'
+import * as core from '@actions/core';
 
 export const versions = {
-    "latest": "1.21.4",
-    "versions": [
-        // Versions must be found from https://hub.spigotmc.org/versions/
-        "1.21.4",
-        "1.21.3",
-        "1.21.2",
-        "1.21.1",
-        "1.21",
+	latest: '26.1.2',
+	versions: [
+		// Versions must be found from https://hub.spigotmc.org/versions/
+		'26.1.2',
+		'26.1.1',
+		'26.1',
 
-        "1.20.6",
-        "1.20.5",
-        "1.20.4",
-        "1.20.3",
-        "1.20.2",
-        "1.20.1",
-        "1.20",
+		'1.21.11',
+		'1.21.10',
+		'1.21.9',
+		'1.21.8',
+		'1.21.7',
+		'1.21.6',
+		'1.21.5',
+		'1.21.4',
+		'1.21.3',
+		'1.21.2',
+		'1.21.1',
+		'1.21',
 
-        "1.19.4",
-        "1.19.3",
-        "1.19.2",
-        "1.19.1",
-        "1.19",
+		'1.20.6',
+		'1.20.5',
+		'1.20.4',
+		'1.20.3',
+		'1.20.2',
+		'1.20.1',
+		'1.20',
 
-        "1.18.2",
-        "1.18.1",
-        "1.18",
+		'1.19.4',
+		'1.19.3',
+		'1.19.2',
+		'1.19.1',
+		'1.19',
 
-        "1.17.1",
-        "1.17",
+		'1.18.2',
+		'1.18.1',
+		'1.18',
 
-        "1.16.5",
-        "1.16.4",
-        "1.16.3",
-        "1.16.2",
-        "1.16.1",
+		'1.17.1',
+		'1.17',
 
-        "1.15.2",
-        "1.15.1",
-        "1.15",
+		'1.16.5',
+		'1.16.4',
+		'1.16.3',
+		'1.16.2',
+		'1.16.1',
 
-        "1.14.4",
-        "1.14.3",
-        "1.14.2",
-        "1.14.1",
-        "1.14",
+		'1.15.2',
+		'1.15.1',
+		'1.15',
 
-        "1.13.2",
-        "1.13.1",
-        "1.13",
+		'1.14.4',
+		'1.14.3',
+		'1.14.2',
+		'1.14.1',
+		'1.14',
 
-        "1.12.2",
-        "1.12.1",
-        "1.12",
+		'1.13.2',
+		'1.13.1',
+		'1.13',
 
-        "1.11.2",
-        "1.11.1",
-        "1.11",
+		'1.12.2',
+		'1.12.1',
+		'1.12',
 
-        "1.10.2",
-        "1.10",
+		'1.11.2',
+		'1.11.1',
+		'1.11',
 
-        "1.9.4",
-        "1.9.2",
-        "1.9",
+		'1.10.2',
+		'1.10',
 
-        "1.8.8",
-        "1.8.7",
-        "1.8.6",
-        "1.8.5",
-        "1.8.4",
-        "1.8.3",
-        "1.8"
-    ],
-    "similar_versions": {
-        "1.21.3": ["1.21.2"],
-        "1.21.1": ["1.21"],
+		'1.9.4',
+		'1.9.2',
+		'1.9',
 
-        "1.20.6": ["1.20.5"],
-        "1.20.4": ["1.20.3"],
-        "1.20.1": ["1.20"],
+		'1.8.8',
+		'1.8.7',
+		'1.8.6',
+		'1.8.5',
+		'1.8.4',
+		'1.8.3',
+		'1.8'
+	],
+	similar_versions: {
+		'21.1.2': ['21.1.1', '21.1'],
 
-        "1.19.1": ["1.19"],
+		'1.21.10': ['1.21.9'],
+		'1.21.8': ['1.21.7', '1.21.6'],
+		'1.21.3': ['1.21.2'],
+		'1.21.1': ['1.21'],
 
-        "1.18.1": ["1.18"],
+		'1.20.6': ['1.20.5'],
+		'1.20.4': ['1.20.3'],
+		'1.20.1': ['1.20'],
 
-        "1.17.1": ["1.17"],
+		'1.19.1': ['1.19'],
 
-        "1.16.5": ["1.16.4"],
-        "1.16.3": ["1.16.2"],
+		'1.18.1': ['1.18'],
 
-        "1.15.2": ["1.15.1", "1.15"],
+		'1.17.1': ['1.17'],
 
-        "1.14.4": ["1.14.3", "1.14.2", "1.14.1", "1.14"],
+		'1.16.5': ['1.16.4'],
+		'1.16.3': ['1.16.2'],
 
-        "1.13.1": ["1.13"],
+		'1.15.2': ['1.15.1', '1.15'],
 
-        "1.12.2": ["1.12.1", "1.12"],
+		'1.14.4': ['1.14.3', '1.14.2', '1.14.1', '1.14'],
 
-        "1.11.2": ["1.11.1", "1.11"],
+		'1.13.1': ['1.13'],
 
-        "1.10.2": ["1.10.1", "1.10"],
+		'1.12.2': ['1.12.1', '1.12'],
 
-        "1.9.4": ["1.9.3", "1.9.2"],
-        "1.9.1": ["1.9"],
+		'1.11.2': ['1.11.1', '1.11'],
 
-        "1.8.9": ["1.8.8", "1.8.7", "1.8.6", "1.8.5", "1.8.4"],
-        "1.8.3": ["1.8.2", "1.8.1", "1.8"],
-        "1.8.2": ["1.8.1", "1.8"]
-    },
+		'1.10.2': ['1.10.1', '1.10'],
 
-    "experimental": [
-        "1.13-pre7",
-        "1.14-pre5",
-        "1.14.3-pre4",
-        "1.18-pre5",
-        "1.18-pre8",
-        "1.18-rc3",
-    ]
-}
+		'1.9.4': ['1.9.3', '1.9.2'],
+		'1.9.1': ['1.9'],
 
-export const latest = versions.latest
-export const current = core.getInput('version').toLowerCase() || latest
-export const experimental = core.getInput('experimental') === "true"
+		'1.8.9': ['1.8.8', '1.8.7', '1.8.6', '1.8.5', '1.8.4'],
+		'1.8.3': ['1.8.2', '1.8.1', '1.8'],
+		'1.8.2': ['1.8.1', '1.8']
+	},
+
+	experimental: ['1.13-pre7', '1.14-pre5', '1.14.3-pre4', '1.18-pre5', '1.18-pre8', '1.18-rc3']
+};
+
+export const latest = versions.latest;
+export const current = core.getInput('version').toLowerCase() || latest;
+export const experimental = core.getInput('experimental') === 'true';
 
 export function similar(version) {
-    for (const v in versions.similar_versions)
-        if (versions.similar_versions[v].includes(version)) return v;
+	for (const v in versions.similar_versions)
+		if (versions.similar_versions[v].includes(version)) return v;
 
-
-    return version;
+	return version;
 }
 
 export function isAvailable(version) {
-    let included = versions.versions.includes(version) || (experimental && versions.experimental.includes(version))
-    if (core.getInput('use-similar-versions') === "true")
-        included = included || versions.versions.includes(similar(version)) || (experimental && versions.experimental.includes(similar(version)))
+	let included =
+		versions.versions.includes(version) ||
+		(experimental && versions.experimental.includes(version));
 
-    return included
+	if ((core.getInput('use-similar-versions').toLowerCase() ?? 'true') === 'true')
+		included =
+			included ||
+			versions.versions.includes(similar(version)) ||
+			(experimental && versions.experimental.includes(similar(version)));
+
+	return included;
 }
--- src/assets/versions/velocity.js
+++ src/assets/versions/velocity.js
@@ -1,22 +1,24 @@
 export const versions = {
-  "latest": "3.4.0-SNAPSHOT",
-  "versions": [
-    "3.4.0-SNAPSHOT",
-    "3.3.0-SNAPSHOT",
-    "3.2.0-SNAPSHOT",
-    "3.1.2-SNAPSHOT",
-    "3.1.1-SNAPSHOT",
+	latest: '3.5.0-SNAPSHOT',
+	versions: [
+		'3.5.0-SNAPSHOT',
+		'3.4.0',
+		'3.4.0-SNAPSHOT',
+		'3.3.0-SNAPSHOT',
+		'3.2.0-SNAPSHOT',
+		'3.1.2-SNAPSHOT',
+		'3.1.1-SNAPSHOT',
 
-    "3.1.1",
-    "3.1.0",
+		'3.1.1',
+		'3.1.0',
 
-    "1.1.9",
-    "1.0.10"
-  ]
-}
+		'1.1.9',
+		'1.0.10'
+	]
+};
 
-export const latest = versions.latest
+export const latest = versions.latest;
 
 export function isAvailable(version) {
-  return versions.versions.includes(version)
+	return versions.versions.includes(version);
 }
