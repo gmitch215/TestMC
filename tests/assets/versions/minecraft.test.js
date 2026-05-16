3734e7aaaa3ccc05b24f98c01a0e26817f83afbf
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2026-05-16 11:40:59 -0500

    test: perform fetch request to ensure versions exist on spigot

--- tests/assets/versions/minecraft.test.js
+++ tests/assets/versions/minecraft.test.js
@@ -1,37 +1,62 @@
-import {expect, test, describe} from '@jest/globals'
+import { describe, expect, test } from '@jest/globals';
 
-import { latest, versions, similar, isAvailable } from '../../../src/assets/versions/minecraft'
+import { isAvailable, latest, similar, versions } from '../../../src/assets/versions/minecraft';
+
+describe('minecraft versions', () => {
+	test('minecraft.versions', () => {
+		// ensure they exist at all
+
+		for (const version of versions.versions) {
+			expect(version).toBeTruthy();
+			expect(isAvailable(version)).toBe(true);
+
+			// perform fetch request
+			fetch(`https://hub.spigotmc.org/versions/${version}.json`)
+				.then((response) => {
+					expect(response.status).toBe(200);
+				})
+				.catch((error) => {
+					// ignore if offline
+					if (error instanceof TypeError) {
+						expect(error.message).toMatch(/Failed to fetch/);
+					} else {
+						throw error;
+					}
+				});
+		}
+	});
+});
 
 describe('minecraft properties', () => {
-    test('minecraft.latest', () => {
-        expect(latest).toBe(versions.versions[0])
-    })
-})
+	test('minecraft.latest', () => {
+		expect(latest).toBe(versions.versions[0]);
+	});
+});
 
 describe('minecraft functions', () => {
-    test('minecraft#similar', () => {
-        expect(similar(latest)).toBe(latest)
+	test('minecraft#similar', () => {
+		expect(similar(latest)).toBe(latest);
 
-        expect(similar('1.20.3')).toBe('1.20.4')
-        expect(similar('1.19')).toBe('1.19.1')
-        expect(similar('1.18')).toBe('1.18.1')
+		expect(similar('1.20.3')).toBe('1.20.4');
+		expect(similar('1.19')).toBe('1.19.1');
+		expect(similar('1.18')).toBe('1.18.1');
 
-        expect(similar('1.17.1')).toBe('1.17.1')
-        expect(similar('1.18.2')).toBe('1.18.2')
-        expect(similar('1.19.4')).toBe('1.19.4')
-    })
+		expect(similar('1.17.1')).toBe('1.17.1');
+		expect(similar('1.18.2')).toBe('1.18.2');
+		expect(similar('1.19.4')).toBe('1.19.4');
+	});
 
-    test('minecraft#isAvailable', () => {
-        expect(isAvailable(latest)).toBe(true)
+	test('minecraft#isAvailable', () => {
+		expect(isAvailable(latest)).toBe(true);
 
-        expect(isAvailable('1.20.5')).toBe(true)
-        expect(isAvailable('1.20.4')).toBe(true)
-        expect(isAvailable('1.19.1')).toBe(true)
-        expect(isAvailable('1.19')).toBe(true)
-        expect(isAvailable('1.18')).toBe(true)
-        expect(isAvailable('1.18.2')).toBe(true)
+		expect(isAvailable('1.20.5')).toBe(true);
+		expect(isAvailable('1.20.4')).toBe(true);
+		expect(isAvailable('1.19.1')).toBe(true);
+		expect(isAvailable('1.19')).toBe(true);
+		expect(isAvailable('1.18')).toBe(true);
+		expect(isAvailable('1.18.2')).toBe(true);
 
-        expect(isAvailable('1.7.10')).toBe(false)
-        expect(isAvailable('2.0')).toBe(false)
-    })
-})
+		expect(isAvailable('1.7.10')).toBe(false);
+		expect(isAvailable('2.0')).toBe(false);
+	});
+});
